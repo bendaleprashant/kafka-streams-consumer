ab791f769bf4053073963dfbeec2b60802781d62
bendaleprashant <bendaleprashant@gmail.com>
2020-08-16 09:32:15 +0530

    Initial commit

---
+++ src/test/java/com/pb/spring/poc/streams/kafka/consumer/KafkaStreamsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pb.spring.poc.streams.kafka.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
